96a68b080e03c441c13dc367ea7c83d287d68420
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2021-12-22 18:29:10 -0300

    Implementado handle para o About dinâmico

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,39 +6,32 @@ function handleData(){
             handleExperience(element.Company,element.Position,element.Period,element.Description))
         retorno.Education.forEach(element => 
             handleEducation(element.Institution, element.Course,element.Status, element.Period))
+        retorno.About.forEach(element =>
+            handleAbout(element.Title, element.Description))
     })
 }
 
+function handleAbout(title, description){
+    const about = document.querySelector("#sobre");
+    const container = document.createElement('div');
+    container.innerHTML = `<span>${title}</span>
+                        <p>${description}</p>`;
+    about.appendChild(container);
+}
+
 function handleEducation(institution, course, status, period){
-
     const education = document.querySelector("#educacao");
-
     const container = document.createElement('div');
     container.innerHTML = `<h3>${course}</h3>
-                            <p>${institution} - ${status} - ${period}</p>`;
-
+                        <p>${institution} - ${status} - ${period}</p>`;
     education.appendChild(container);
-
 }
 
 function handleExperience(company, position, period, description){
-
-    const experiencias = document.querySelector("#experiencias-profissionais");
-
-    const title = document.createElement('h3');
-    title.innerText = `${position} @${company}`;
-
-    const dates = document.createElement('h4');
-    dates.innerText = `${period}`;
-
-    const desc = document.createElement('p');
-    desc.innerText = `${description}`;
-
+    const experiencies = document.querySelector("#experiencias-profissionais"); 
     const container = document.createElement('div');
-    container.appendChild(title);
-    container.appendChild(dates);
-    container.appendChild(desc);
-
-    experiencias.appendChild(container);
-
+    container.innerHTML = `<h3>${position}@${company}</h3>
+                        <h4>${period}</h4>
+                        <p>${description}</p>`;
+    experiencies.appendChild(container);
 }
