d9690503448123abda7aa3736a9c470cb117c4e0
Augusto Madeira Splett <augusto.splett@gmail.com>
2023-08-11 20:20:47 -0400

    FIX/ problems to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,13 +23,13 @@ export default function Home() {
 
           
           <p>
-            Hello there! I'm thrilled to share my journey with you. Over the past six dynamic years in the technology industry, I've gained valuable experience that showcases my adaptability and eagerness to learn. I've had the opportunity to explore diverse sectors like finance, services, and outsourcing companies, allowing me to seamlessly embrace new challenges and technologies.
+            Hello there! I&apos;m thrilled to share my journey with you. Over the past six dynamic years in the technology industry, I&apos;ve gained valuable experience that showcases my adaptability and eagerness to learn. I&apos;ve had the opportunity to explore diverse sectors like finance, services, and outsourcing companies, allowing me to seamlessly embrace new challenges and technologies.
           </p>
           <p>
-            My real passion lies in problem-solving and coding. As a junior enthusiast, I'm currently immersed in the world of JavaScript, React, NextJS, and C#. With each line of code, I'm taking strides towards becoming a skilled developer. While my journey is still in its early stages, I'm armed with a toolkit of determination and a willingness to dive deep into projects, absorbing every learning opportunity along the way.
+            My real passion lies in problem-solving and coding. As a junior enthusiast, I&apos;m currently immersed in the world of JavaScript, React, NextJS, and C#. With each line of code, I&apos;m taking strides towards becoming a skilled developer. While my journey is still in its early stages, I&apos;m armed with a toolkit of determination and a willingness to dive deep into projects, absorbing every learning opportunity along the way.
           </p>
           <p>
-            Thank you for joining me on this exciting path of growth and development. Through my portfolio, you'll witness not only my projects but also my unwavering commitment to evolving as a developer. Let's embrace the coding adventures ahead and turn aspirations into accomplishments, one line of code at a time!
+            Thank you for joining me on this exciting path of growth and development. Through my portfolio, you&apos;ll witness not only my projects but also my unwavering commitment to evolving as a developer. Let&apos;s embrace the coding adventures ahead and turn aspirations into accomplishments, one line of code at a time!
           </p>
         </Section>
         <Section>
